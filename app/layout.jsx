270e2569a489a465cfd88acff21c84b7c4804b5a
heir04 <ismailayomide32@gmail.com>
2025-09-24 12:47:28 +0100

    Add Vercel Analytics for improved performance tracking

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import ClientWrapper from './ClientWrapper'
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
